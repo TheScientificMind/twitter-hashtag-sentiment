4d620d8210f503d8aaff563d93df65b99a1863e1
Dylan Headley <dylanmatthewheadley@gmail.com>
2023-03-20 22:02:55 -0400

    Added try/except block

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ import sys
 
 # ensures the file isn't being imported as a module
 if __name__ == "__main__":
-    hashtag = input("What hashtag would you like to analyze (e.g. #photography): ").lower().strip()
+    try:    
+        hashtag = input("What hashtag would you like to analyze (e.g. #photography): ").lower().strip()
 
-    # invalid hashtag causes program to stop
-    if not hashtag[1:].isalnum() or hashtag[1:].isnumeric() or "#" not in hashtag:
-        print("Your hashtag was invalid. Please run the program and try again.")
-        sys.exit()
+        # invalid hashtag causes program to stop
+        if not hashtag[1:].isalnum() or hashtag[1:].isnumeric() or "#" not in hashtag:
+            print("Your hashtag was invalid. Please run the program and try again.")
+            sys.exit()
+    except Exception as err:
+        print(f"Your input caused the error:\n\n{err}\n\nPlease try again.")
         
