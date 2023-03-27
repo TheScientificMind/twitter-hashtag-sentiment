908e5fe3749779d7166ff35a24feb687117895f5
Dylan Headley <dylanmatthewheadley@gmail.com>
2023-03-26 20:30:38 -0400

    Split dataset into training and validation and linked sources 4 ltr use

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import sys
 import pandas as pd
+import numpy as np
+from numpy.random import RandomState
+import tensorflow as tf
 # import matplotlib.pyplot as plt
 # import seaborn as sns
-# import numpy as np
-# import tensorflow as tf
 # import spacy
 # import re
 # import tweepy
 
+# good resource: https://www.youtube.com/watch?v=lKoPIQdRBME&ab_channel=MachineLearningwithPhil
+
 # ensures the file isn't being imported as a module
 if __name__ == "__main__":
     try:
-        df = pd.read_csv(r'C:\Users\dylan\Downloads\tweetsdataset.csv')
-        print(df)
+        df = pd.read_csv("tweetsdataset.csv")
+        
+        # lines 16-20 from https://stackoverflow.com/questions/43697240
+        rng = RandomState()
+        train = df.sample(frac=0.9, random_state=rng)
+        test = df.loc[~df.index.isin(train.index)]
 
         hashtag = input("What hashtag would you like to analyze (e.g. #photography): ").lower().strip()
 
