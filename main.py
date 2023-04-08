d78399b28e239927ac239e9e14b8f927f06e089d
Dylan Headley <dylanmatthewheadley@gmail.com>
2023-04-07 22:56:15 -0400

    Finished building and evaluating model. Left bufixes for later.

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ train_text, train_labels = train["text"], train["label"]
 test = tweets[split_point:]
 test_text, test_labels = test["text"], test["label"]
 
+# layer to convert words into ints
 vectorizer = layers.TextVectorization(
     max_tokens = 2500,
     output_mode = "int", 
@@ -37,12 +38,10 @@ vectorizer = layers.TextVectorization(
     pad_to_max_tokens = True
 )
 
-print(train_text.dtypes)
-exit()
+vectorizer.adapt(train_text) # converts train_text to ints
+vectorizer.adapt(test_text) # converts test_text to ints
 
-vectorizer.adapt(train_text)
-vectorizer.adapt(test_text)
-
+# building the model
 model = keras.Sequential([
     keras.layers.Embedding(10000, 16, input_length = 200),
     keras.layers.GlobalAveragePooling1D(),
@@ -50,6 +49,34 @@ model = keras.Sequential([
     keras.layers.Dense(1, activation = "sigmoid")
 ])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# compiling the model
+model.compile(
+    optimizer='adam', 
+    loss='binary_crossentropy', 
+    metrics=['accuracy']
+) 
 
-model.summary()
+# training the model
+history = model.fit(
+    train_text, 
+    train_labels, 
+    epochs = 25, 
+    batch_size = 512, 
+    validation_data = (test_text, test_labels)
+)
+
+loss, accuracy = model.evaluate(test_text, test_labels) # evaluating model loss/accuracy
+
+# check if the output is correct
+index = np.random.randint(1,1000)
+user_review = test.loc[index]
+print(user_review)
+
+user_review = test_text[index]
+user_review = np.array([user_review])
+if (round(model.predict(user_review)) == 1).astype("int32"):
+  print("positive sentiment")
+elif (round(model.predict(user_review)) == -1).astype("int32"):
+  print("negative sentiment")
+else:
+  print("neutral sentiment")
